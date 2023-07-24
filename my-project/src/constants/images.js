91eb83ebba59a332142602e3ff7bc3fef76e503b
Zac <zac.upshure@outlook.com>
2023-07-25 01:29:10 +0200

    populated with some images

--- my-project/src/constants/images.js
+++ my-project/src/constants/images.js
@@ -1,5 +1,15 @@
 import olibaustoffe from './../assets/OliBaustoffe.png';
+import TextAaron from './../assets/3dTextAaron.png';
+import fixEnergyOne from './../assets/fixEnergy1.png';
+import fixEnergyTwo from './../assets/fixEnergy2.png';
+import fixEnergyThree from './../assets/fixEnergy3.png';
+import vw_gti from './../assets/vw_gti_shooting.png';
 
 export default {
-    olibaustoffe
+    olibaustoffe,
+    TextAaron,
+    fixEnergyOne,
+    fixEnergyTwo,
+    fixEnergyThree,
+    vw_gti,
 };
